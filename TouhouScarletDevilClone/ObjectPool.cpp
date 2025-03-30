63890c508fc037296c68308674ebfc88b2c091df
alzseven <jhp7140@gmail.com>
2025-03-30 22:49:14 +0900

    Add test pool

---
+++ TouhouScarletDevilClone/ObjectPool.cpp
@@ -0,0 +1,36 @@
+﻿#include "ObjectPool.h"
+
+template <typename T>
+void ObjectPool<T>::Init(size_t preAlloc)
+{
+    pool.reserve(preAlloc);
+    for(size_t i=0; i<preAlloc; ++i) {
+        pool[i].isActive = false;
+        freeList.push(i);
+    }
+}
+
+
+template <typename T>
+T* ObjectPool<T>::Allocate()
+{
+    if(freeList.empty()) {
+        size_t newSize = pool.size() * 2;
+        pool.resize(newSize);
+        for(size_t i=pool.size()/2; i<newSize; ++i)
+            freeList.push(i);
+    }
+        
+    size_t index = freeList.top();
+    freeList.pop();
+    pool[index].isActive = true;
+    return &pool[index];
+}
+
+template <typename T>
+void ObjectPool<T>::Release(T* obj)
+{
+    obj->Reset();
+    obj->isActive = false;
+    freeList.push(obj - &pool[0]);
+}
---
+++ TouhouScarletDevilClone/ObjectPool.h
@@ -0,0 +1,26 @@
+﻿// ObjectPool.h
+#pragma once
+#include <stack>
+
+#include "config.h"
+
+template<typename T>
+class ObjectPool {
+    std::vector<T> pool;
+    stack<size_t> freeList;
+    
+public:
+    ObjectPool() = default;
+    ~ObjectPool() = default;
+
+    void Init(size_t preAlloc = 1000);
+    
+    T* Allocate();
+
+    void Release(T* obj);
+
+    // auto GetActiveObjects() {
+    //     return pool | std::views::filter([](const T& o){ return o.isActive; });
+    // }
+};
+
