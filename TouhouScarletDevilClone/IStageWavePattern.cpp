9d5a7d5bfc12d488c57837971e8b1eb43d7433bd
alzseven <jhp7140@gmail.com>
2025-04-02 03:53:00 +0900

    __TEMP

---
+++ TouhouScarletDevilClone/IStageWavePattern.cpp
@@ -0,0 +1,32 @@
+﻿#include "IStageWavePattern.h"
+
+void State1Wave1Pattern::Update(float deltaTime)
+{
+
+    
+}
+
+void State1Wave1Pattern::Spawn()
+{
+}
+
+void State1Wave1Pattern::Init()
+{
+    patternStartTime;
+    patternEndTime;
+    enemyToSpawn;
+
+    //Shoot params
+    spawnDelay;
+    spawnAmount;
+    spawnAngle;
+    spawnAngleRate;
+    // float spawnSpeed;
+    // float spawnSpeedRate;
+    multiSpawnDelay;
+    
+    shootTimer;
+
+
+    timeElpased;
+}
---
+++ TouhouScarletDevilClone/IStageWavePattern.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+class BHEnemy;
+
+class IStageWavePattern
+{
+    float patternStartTime;
+    float patternEndTime;
+    BHEnemy* enemyToSpawn;
+
+    //Shoot params
+    float spawnDelay;
+    int spawnAmount;
+    float spawnAngle;
+    float spawnAngleRate;
+    // float spawnSpeed;
+    // float spawnSpeedRate;
+    float multiSpawnDelay;
+    
+    float shootTimer;
+
+
+    float timeElpased;
+public:
+    virtual void Spawn() = 0;
+    virtual void Update(float deltaTime) = 0;
+};
+
+class State1Wave1Pattern : public IStageWavePattern
+{
+    int currentSpawnCount;
+    
+    void Update(float deltaTime) override;
+    void Spawn() override;
+
+    //Test
+    void Init();
+};
